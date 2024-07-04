f2c51828ae2497430abb1778165445dee0363bcc
gwangwon <lg25sk@gmail.com>
2024-07-04 23:04:36 +0900

    asd

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,43 @@
 <template>
-  <div class="header">
-    <ul class="header-button-left">
-      <li>Cancel</li>
-    </ul>
-    <ul class="header-button-right">
-      <li>Next</li>
-    </ul>
-    <img src="./assets/logo.png" class="logo" />
+  <div>
+    <div class="header">
+      <ul class="header-button-left">
+        <li @click="step = 0">Cancel</li>
+      </ul>
+      <ul class="header-button-right">
+        <li @click="nextStep">Next</li>
+      </ul>
+      <img src="./assets/logo.png" class="logo" />
+    </div>
+    
+    <button @click="step = 0">버튼1</button>
+    <button @click="step = 1">버튼2</button>
+    <button @click="step = 2">버튼3</button>
+    
+    <Container :instaData="instaData" :step="step" />
+    
+    <button class="the" @click="more">더보기</button>
+    
+    <div class="footer">
+      <ul class="footer-button-plus">
+        <input type="file" id="file" class="inputfile" />
+        <label for="file" class="input-plus">+</label>
+      </ul>
+    </div>
   </div>
-  <Container :instaData="instaData"/>
-  <button class="the" @click="more">더보기</button>
-  <div class="footer">
-    <ul class="footer-button-plus">
-      <input type="file" id="file" class="inputfile" />
-      <label for="file" class="input-plus">+</label>
-    </ul>
- </div>
 </template>
+
 <script>
-import Container from './components/Container.vue'
+import Container from './components/Container.vue';
 import data from './assets/data.js'
 import axios from 'axios';
 export default {
-  name: 'App',
   data() {
     return {
+      step: 0,
       instaData:data,
-      clickNum : 0,
-    }
-  },
-  components: {
-    Container,
+      clickNum:0
+    };
   },
   methods:{
     more(){
@@ -44,9 +51,11 @@ export default {
           console.log(err)
         })
     }
-  }
+  },
+  components: {
+    Container,
+  },
 }
-
 </script>
 
 <style>
@@ -141,3 +150,4 @@ ul {
   background-color: #007bff; 
 }
 </style>
+
